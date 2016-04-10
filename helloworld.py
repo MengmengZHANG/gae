52f7f4b127617153137855455c950b30a130dd6b
Mengmeng ZHANG <qiuyu8290@gmail.com>
2016-04-10 12:05:26 +0000

    test from google cloud platform

--- helloworld.py
+++ helloworld.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello World from google cloud platform!'
 
 
 if __name__ == '__main__':
